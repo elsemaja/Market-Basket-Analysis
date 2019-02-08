76074722c7a3f87cbc3ef7e66b064df9b47136b3
Else Frijling <else@MacBook-Pro-van-Else.local>
2019-02-08 13:15:11 +0100

    Final

--- scripts/1.Libraries and dataframe market basket analysis_V2.R
+++ scripts/1.Libraries and dataframe market basket analysis_V2.R
@@ -14,19 +14,20 @@ library("trimcluster")
 library("graphics")
 library("RColorBrewer")
 
+# Set working directory
+setwd("/Users/else/Google Drive/BSSA - data analytics/Module 2/Task 4 - market basket analysis/Market Basket Analysis")
 
 # upload data using read.transactions(),
 # create two dataset Transactions based on product (=default) and
 # Transactions based on category "TransactionsCat"
-
-Transactions <- read.transactions("ElectronidexTransactions2017.csv",
+Transactions <- read.transactions("data/ElectronidexTransactions2017.csv",
                                   format = "basket",
                                   sep = ",",
                                   skip = 0,
                                   rm.duplicates = TRUE)
 
 
-TransactionsCat <- read.transactions("ElectronidexTransactions2017 categories.csv",
+TransactionsCat <- read.transactions("data/ElectronidexTransactions2017 categories.csv",
                                   format = "basket",
                                   sep = ";",
                                   skip = 0,
